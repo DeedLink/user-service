23e81e9673eeb7c7269109da9d6bbd2f0eb0b8a3
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-08-27 10:13:43 +0530

    ipfs created

--- src/utils/ipfs.js
+++ src/utils/ipfs.js
@@ -1,14 +1,52 @@
 import { create } from "ipfs-http-client";
+import axios from "axios";
+import FormData from "form-data";
 
-const projectId = process.env.IPFS_PROJECT_ID;
-const projectSecret = process.env.IPFS_PROJECT_SECRET;
-const auth = "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+const useInfura = process.env.USE_INFURA === "true";
 
-export const ipfsClient = create({
-  host: "ipfs.infura.io",
-  port: 5001,
-  protocol: "https",
-  headers: {
-    authorization: auth,
-  },
-});
+let ipfsClient;
+
+if (useInfura) {
+  const projectId = process.env.IPFS_PROJECT_ID;
+  const projectSecret = process.env.IPFS_PROJECT_SECRET;
+  const auth =
+    "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+
+  ipfsClient = create({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
+    headers: {
+      authorization: auth,
+    },
+  });
+} else {
+  const microserviceURL =
+    process.env.IPFS_MICROSERVICE_URL || "http://localhost:4000";
+
+  ipfsClient = {
+    add: async (file) => {
+      const formData = new FormData();
+      formData.append("file", file, "kycDocument.pdf");
+
+      const res = await axios.post(`${microserviceURL}/upload`, formData, {
+        headers: formData.getHeaders(),
+      });
+
+      return {
+        path: res.data.hash,
+        cid: res.data.hash,
+        url: res.data.url,
+      };
+    },
+
+    cat: async (hash) => {
+      const res = await axios.get(`${microserviceURL}/file/${hash}`, {
+        responseType: "arraybuffer",
+      });
+      return res.data;
+    },
+  };
+}
+
+export { ipfsClient };
